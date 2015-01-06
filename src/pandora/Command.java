5dc697473aab928bbf4a371edd78de61edf7efd2
Abdel Karki <abdel.karki@gmail.com>
2015-01-06 11:52:34 +0100

    Directions cardinales finies

--- src/pandora/Command.java
+++ src/pandora/Command.java
@@ -4,14 +4,26 @@
  * and open the template in the editor.
  */
 package pandora;
-import java.util.HashMap;
 /**
  *
  * @author Cyril, Abdelrahmane
  */
 public class Command {
-    public void goNorth (HashMap currentRoom)
+
+    public void goNorth (Room actualRoom, Player Player1)
     {
-        
+        Player1.move(actualRoom.getDirection("North"));
+    }
+    public void goSouth (Room actualRoom, Player Player1)
+    {
+        Player1.move(actualRoom.getDirection("South"));
+    }
+    public void goEast (Room actualRoom, Player Player1)
+    {
+        Player1.move(actualRoom.getDirection("East"));
+    }
+    public void goWest (Room actualRoom, Player Player1)
+    {
+        Player1.move(actualRoom.getDirection("West"));
     }
 }
