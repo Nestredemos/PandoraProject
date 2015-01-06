e35c4147925e0cde47d63311274967984159d3ff
Cyril <Cyril@Cyril-PC>
2015-01-06 14:37:37 +0100

    Commit codePropre

--- src/pandora/Item.java
+++ src/pandora/Item.java
@@ -1,20 +1,36 @@
 package pandora;
 
+import java.util.ArrayList;
+
 public class Item {
 
   private final String nameItem;
+  
 
   public Item(String name) {
       this.nameItem = name;
   }
 
-  public void moreItem() {
+  public void moveItem(ArrayList<Item> containedItem, ArrayList<Item> Inventory, String nameNewItem){
+      for(Item test : containedItem){
+          if(test.nameItem.equals(nameNewItem)){
+            Inventory.add(test);
+          }
+      }
   }
 
-  public void destroyItem() {
+  public void destroyItem(ArrayList<Item> inventory, Item oldItem){
+      for(Item test : inventory){
+          if(test.equals(oldItem)){
+              inventory.remove(test);
+          }
+      }
   }
 
-  public void showItem() {
+  public void showItem(ArrayList<Item> inventory){
+      for(Item test : inventory){
+          System.out.println(test.nameItem);
+      }
   }
 
 }
